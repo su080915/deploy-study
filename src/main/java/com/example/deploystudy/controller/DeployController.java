225de7489dc144caaf82b34c51b86f354a9341af
su080915 <tbr335@naver.com>
2024-09-17 10:58:10 +0900

    chore::try7

--- src/main/java/com/example/deploystudy/controller/DeployController.java
+++ src/main/java/com/example/deploystudy/controller/DeployController.java
@@ -4,6 +4,7 @@ package com.example.deploystudy.controller;
 import com.example.deploystudy.service.DeployService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DeployController {
     private final DeployService deployService;
 
-    @GetMapping("/here")
+    @PostMapping("/here")
     public void index() {
         deployService.save();
     }
