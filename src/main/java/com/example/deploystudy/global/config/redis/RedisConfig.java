e785a3e9ec862e3c8393c6af3bdeea27b84679c7
su080915 <tbr335@naver.com>
2024-09-23 22:01:49 +0900

    feat: redis properties

--- src/main/java/com/example/deploystudy/global/config/redis/RedisConfig.java
+++ src/main/java/com/example/deploystudy/global/config/redis/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.example.deploystudy.global.config.redis;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
