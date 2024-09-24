fbaf6a00f607ad5e10fc562b62574b95a574601a
su080915 <tbr335@naver.com>
2024-09-24 23:45:59 +0900

    feat: admin post

--- src/main/java/com/example/deploystudy/domain/admin/controller/AdminController.java
+++ src/main/java/com/example/deploystudy/domain/admin/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.example.deploystudy.domain.admin.controller;
 
 
 import com.example.deploystudy.domain.admin.service.AdminService;
+import com.example.deploystudy.domain.post.domain.Post;
 import com.example.deploystudy.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +17,13 @@ import java.util.List;
 public class AdminController {
     private final AdminService adminService;
 
-    @GetMapping
+    @GetMapping("/users")
     public List<User> getUsers() {
         return adminService.getUsers();
+    }
+
+    @GetMapping("/posts")
+    public List<Post> getPosts() {
+        return adminService.getPosts();
     }
 }
--- src/main/java/com/example/deploystudy/domain/admin/service/AdminService.java
+++ src/main/java/com/example/deploystudy/domain/admin/service/AdminService.java
@@ -1,6 +1,8 @@
 package com.example.deploystudy.domain.admin.service;
 
 
+import com.example.deploystudy.domain.post.domain.Post;
+import com.example.deploystudy.domain.post.repository.PostRepository;
 import com.example.deploystudy.domain.user.domain.User;
 import com.example.deploystudy.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +14,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AdminService {
     private final UserRepository userRepository;
+    private final PostRepository postRepository;
 
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    public List<Post> getPosts() {
+        return postRepository.findAll();
     }
 }
